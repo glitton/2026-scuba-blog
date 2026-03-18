af27787815a9c6bf533ca38ddffce6ab87eb5195
Generosa Litton <glcodeworks@gmail.com>
2026-03-18 16:42:33 -0500

    Added home link to glcodeworks.com

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,5 +1,5 @@
 const headerNavLinks = [
-  { href: '/', title: 'Home' },
+  { href: 'https://glcodeworks.com', title: 'Home' },
   { href: '/stories', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   // { href: '/projects', title: 'Projects' },
